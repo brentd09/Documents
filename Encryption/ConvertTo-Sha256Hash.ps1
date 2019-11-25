825f20a27954ad35ee412d6021f557a1d5290613
Brent Denny <brent.denny@ddls.com.au>
2019-11-26 04:37:47 +1000

    fix hash converter

--- Encryption/ConvertTo-Sha256Hash.ps1
+++ Encryption/ConvertTo-Sha256Hash.ps1
@@ -3,7 +3,7 @@ Param (
   [Parameter(Mandatory=$true)]
   [string]$DataToBeHashed
 )
-new-object System.Security.Cryptography.SHA256Managed | 
+New-Object System.Security.Cryptography.SHA256Managed | 
 ForEach-Object {
   $_.ComputeHash([System.Text.Encoding]::UTF8.GetBytes($DataToBeHashed))
 } | ForEach-Object {$_.ToString("x2")} | Write-Host -NoNewline
